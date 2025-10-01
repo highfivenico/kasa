d7961135c2d731c125b854f9b9bfe65d7898e596
Nicolas HighFive <highfivenico@gmail.com>
2025-10-01 10:11:45 +0200

    chore: adjust Collapse component and style effect

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,26 +1,40 @@
-import { useState } from "react";
 import arrow from "../assets/img/arrow-up.png";
+import { useState } from "react";
 
 const Collapse = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleCollapse = () => {
+  const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
   return (
     <div className="collapse">
-      <div className="collapse__header">
-        <h2>{title}</h2>
-        <div className="collapse__toggle" onClick={toggleCollapse}>
-          <img
-            src={arrow}
-            className={`collapse__arrow ${isOpen ? "rotated" : ""}`}
-          ></img>
+      <div className="collapse__item">
+        <div
+          className={`collapse__header ${
+            isOpen ? "collapse__header--active" : ""
+          }`}
+        >
+          <h2>{title}</h2>
+          <div
+            className="collapse__toggle"
+            onClick={(e) => {
+              e.preventDefault();
+              handleToggle(0);
+            }}
+          >
+            <img
+              src={arrow}
+              className={`collapse__arrow ${isOpen ? "rotated" : ""}`}
+            ></img>
+          </div>
         </div>
-      </div>
-      <div className={`collapse__content ${isOpen ? "open" : ""}`}>
-        <div className="collapse__inner">{children}</div>
+        <div className="collapse__wrapper">
+          <div className="collapse__content">
+            <div className="collapse__inner">{children}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
